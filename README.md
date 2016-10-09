39b69fc085aad4a7831d6a9cd7459ae7da8de99f
agreg0ire <artgreg@outlook.fr>
2016-10-09 10:36:24 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#JWT Authentication
+
+>Simple nodeJs Json Web Token Flow authentication
+
+[Official introduction](https://jwt.io/introduction/)
+
+:bulb: Easier to forge your requests with [Postman](https://www.getpostman.com/) than cURL
---
+++ index.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const secret = 'YOUR_SECRET';
+const app = express();
+
+app.use(expressJwt({ secret }).unless({ path: ['/login'] }));
+app.use(bodyParser.urlencoded());
+
+// the private route you can reach on a GET method with POSTMAN (with and without the token)
+app.get('/secured', (req, res) => {
+  const securedInfos = {
+    foo: 'bar',
+    john: 'doe'
+  }
+  res.status(200).json(securedInfos);
+});
+
+// the public route you can reach on a POST method with POSTMAN to retrieve the token
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  const token = jwt.sign({ username: req.body.username }, secret);
+  res.status(200).json(token);
+});
+
+app.listen(3000, () => console.log('listening on 3000'));
